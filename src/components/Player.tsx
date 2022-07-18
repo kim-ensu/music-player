f4fbd4519e3fe3b521c2fda051ed394b27e37c0f
dkimd5 <dkimd5@mail.ru>
2022-07-18 18:38:53 +0300

    Add sections for player

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -4,7 +4,20 @@ import "./Player.css";
 type Props = {};
 
 const Player: FC<Props> = (props) => {
-  return <div>Player</div>;
+  return (
+    <div className="player">
+      <div className="player__current-song"></div>
+      <div className="player__control-panel">
+        <div className="player__timeline-waveform"></div>
+        <div className="player__buttons">
+          <button className="backward"></button>
+          <button className="play-pause"></button>
+          <button className="forward"></button>
+        </div>
+      </div>
+      <div className="player__volume"></div>
+    </div>
+  );
 };
 
 export default Player;
