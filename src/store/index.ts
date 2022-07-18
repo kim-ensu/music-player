6a4f465d41fb14ea071c8fd8919a96247d9a866d
dkimd5 <dkimd5@mail.ru>
2022-07-18 22:16:42 +0300

    Create slice for genres

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import genresReducer from "./slices/genresSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    genres: genresReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
