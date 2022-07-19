c91295e1520f74ffe63ee6098119f270462f31c4
dkimd5 <dkimd5@mail.ru>
2022-07-19 14:56:52 +0300

    Add width height to track card

--- src/components/TrackCard.tsx
+++ src/components/TrackCard.tsx
@@ -10,7 +10,7 @@ type Props = {
 const TrackCard: FC<Props> = ({ track: { name, singer, album_img } }) => {
   return (
     <li>
-      <img src={album_img} alt="track cover" />
+      <img src={album_img} alt="track cover" width={150} height={150} />
       <h1>{name}</h1>
       <h2>{singer}</h2>
     </li>
