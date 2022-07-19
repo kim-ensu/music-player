5c4c46c03fbdc9617bcd6e0fde00cfa2088d5a36
dkimd5 <dkimd5@mail.ru>
2022-07-19 20:06:11 +0300

    Add hamburger icons toggle for genres menu

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,12 +1,17 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { useAppDispatch, useAppSelector } from "store/hooks";
 import { changeGenreId } from "store/slices/genresSlice";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 type Props = {};
 
 const Sidebar: FC<Props> = (props) => {
   const genresList = useAppSelector((state) => state.genres.genresList);
   const dispatch = useAppDispatch();
+
+  const [click, setClick] = useState<boolean>(false);
+
+  const handleMenuToggle = () => setClick(!click);
 
   const handleClick = (genreId: number) => {
     dispatch(changeGenreId(genreId));
@@ -21,6 +26,13 @@ const Sidebar: FC<Props> = (props) => {
           </li>
         ))}
       </ul>
+      <div className="hamburger" onClick={handleMenuToggle}>
+        {click ? (
+          <FaTimes size={20} className="hamburger-color" />
+        ) : (
+          <FaBars size={20} className="hamburger-color" />
+        )}
+      </div>
     </div>
   );
 };
