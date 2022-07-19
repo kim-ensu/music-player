caa964a530e02ac0246bccaa3ed84ddc60e69b97
dkimd5 <dkimd5@mail.ru>
2022-07-19 09:55:41 +0300

    Add audio tag and timeline

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,18 +1,26 @@
 import React, { FC } from "react";
+import { useSelector } from "react-redux";
+import type { RootState } from "store/index";
 import "./Player.css";
 
 type Props = {};
 
 const Player: FC<Props> = (props) => {
+  const count = useSelector((state: RootState) => state.music.musicList[0]);
   return (
     <div className="player">
       <div className="player__current-song"></div>
       <div className="player__control-panel">
-        <div className="player__timeline-waveform"></div>
+        <audio src={count.audio} preload="metadata"></audio>
+        <div className="player__timeline-waveform">
+          <div>0:00</div>
+          <input type="range" />
+          <div>2:49</div>
+        </div>
         <div className="player__buttons">
-          <button className="backward"></button>
-          <button className="play-pause"></button>
-          <button className="forward"></button>
+          <button className="backward">previous</button>
+          <button className="play-pause">play pause</button>
+          <button className="forward">next</button>
         </div>
       </div>
       <div className="player__volume"></div>
