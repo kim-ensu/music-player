765d4f1b844df87d807a50d998f2d10f5641d2ee
dkimd5 <dkimd5@mail.ru>
2022-07-19 14:54:23 +0300

    Create TrackCard component

--- src/components/Playlist.tsx
+++ src/components/Playlist.tsx
@@ -1,10 +1,19 @@
 import React, { FC } from "react";
+import { useSelector } from "react-redux";
+import type { RootState } from "store/index";
 import "./Playlist.css";
 
 type Props = {};
 
 const Playlist: FC<Props> = (props) => {
-  return <div>Playlist</div>;
+  const playlistArr = useSelector((state: RootState) => state.music.musicList);
+  return (
+    <div className="playlist">
+      {playlistArr.map((track) => (
+        <TrackCard />
+      ))}
+    </div>
+  );
 };
 
 export default Playlist;
---
+++ src/components/TrackCard.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { FC } from "react";
+import "./TrackCard.css";
+import { ITrack } from "models/models";
+
+type Props = {
+  track: ITrack;
+};
+
+const TrackCard: FC<Props> = ({ track: { name, singer, album_img } }) => {
+  return (
+    <li>
+      <img src={album_img} alt="track cover" />
+      <h1>{name}</h1>
+      <h2>{singer}</h2>
+    </li>
+  );
+};
+
+export default TrackCard;
