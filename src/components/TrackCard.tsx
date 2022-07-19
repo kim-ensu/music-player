c673424cdf0256aec0010362ddbdf9806b0e19c5
dkimd5 <dkimd5@mail.ru>
2022-07-19 16:22:43 +0300

    Add onClick handler for card

--- src/components/TrackCard.tsx
+++ src/components/TrackCard.tsx
@@ -2,14 +2,22 @@ import React from "react";
 import { FC } from "react";
 import "./TrackCard.css";
 import { ITrack } from "models/models";
+import { useAppDispatch } from "store/hooks";
+import { changeCurrentTrackId } from "store/slices/musicSlice";
 
 type Props = {
   track: ITrack;
 };
 
-const TrackCard: FC<Props> = ({ track: { name, singer, album_img } }) => {
+const TrackCard: FC<Props> = ({ track: { name, singer, album_img, id } }) => {
+  const dispatch = useAppDispatch();
+
+  const handleClick = () => {
+    dispatch(changeCurrentTrackId(id));
+  };
+
   return (
-    <li className="playlist_item">
+    <li className="playlist_item" onClick={handleClick}>
       <div className="playlist_img-holder">
         <img src={album_img} alt="track cover" width={150} height={150} />
       </div>
