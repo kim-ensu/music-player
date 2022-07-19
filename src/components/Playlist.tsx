341fc4e42509b9042070e1025df6de8431fa549f
dkimd5 <dkimd5@mail.ru>
2022-07-19 19:30:04 +0300

    Add filter condition for playlist

--- src/components/Playlist.tsx
+++ src/components/Playlist.tsx
@@ -2,11 +2,18 @@ import React, { FC } from "react";
 import TrackCard from "./TrackCard";
 import "./Playlist.css";
 import { useAppSelector } from "store/hooks";
+import { stat } from "fs";
 
 type Props = {};
 
 const Playlist: FC<Props> = (props) => {
-  const playlistArr = useAppSelector((state) => state.music.musicList);
+  const playlistArr = useAppSelector((state) =>
+    currentGenreId === 0
+      ? state.music.musicList
+      : state.music.musicList.filter((track) => track.genreId === currentGenreId)
+  );
+  const currentGenreId = useAppSelector((state) => state.genres.currentGenreId);
+
   return (
     <div className="playlist">
       <ul>
